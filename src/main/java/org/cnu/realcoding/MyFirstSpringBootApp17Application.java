b34d8da983dfad94ffa546144b97486289cc83ff
shellboy <qkrdmstlr3@naver.com>
2021-04-03 10:51:56 +0900

    [chore] 스프링 부트 생성
    - spring initializr (https://start.spring.io/)를 통한 스프링 부트 생성

---
+++ src/main/java/org/cnu/realcoding/MyFirstSpringBootApp17Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootApp17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootApp17Application.class, args);
+	}
+
+}
